79f422af351670042bf278e51fbd645254d60365
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2024-10-17 09:28:01 -0700

    adding requirements file

--- rf.py
+++ rf.py
@@ -14,11 +14,12 @@ y = data['quality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
+
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-
+print(X_train_scaled[0])
 rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
 rf_model.fit(X_train_scaled, y_train)
 
